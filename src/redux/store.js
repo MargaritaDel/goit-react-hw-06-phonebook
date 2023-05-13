bca16112165578f4ed9d2036be2e29e757777ebd
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-05-13 07:57:55 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,7 +10,14 @@ import {
   persistStore,
 } from 'redux-persist';
 import persistReducer from 'redux-persist/es/persistReducer';
-import { reducer } from './reducer';
+import { combineReducers } from 'redux';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+export const reducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
 
 const persistConfig = {
   key: 'contacts',
@@ -22,6 +29,7 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
